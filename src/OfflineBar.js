899513505380ad09423a23c045e858203da05fd8
stones4semper <eloike95@gmail.com>
2024-07-06 14:09:54 +0100

    fixed some bugs

--- src/OfflineBar.js
+++ src/OfflineBar.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState, useRef, useMemo, useCallback } from 'react'
 import { SafeAreaView, StatusBar, Animated, Easing, StyleSheet } from 'react-native';
 import NetInfo from '@react-native-community/netinfo';
 
-const OfflineBar = ({bgColor='#930F1F', color="#fff", title='Please connect to Wi-Fi or cellular data to go online.'}) => {
-	const animationConstants = useMemo(
-		() => ({
-			DURATION: 800,
-			TO_VALUE: 4,
-			INPUT_RANGE: [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
-			OUTPUT_RANGE: [0, -15, 0, 15, 0, -15, 0, 15, 0],
-		}), [],
-	);
+const OfflineBar = ({ bgColor = '#930F1F', color = '#fff', title = 'Please connect to Wi-Fi or cellular data to go online.' }) => {
+	const animationConstants = useMemo(() => ({
+		DURATION: 800,
+		TO_VALUE: 4,
+		INPUT_RANGE: [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4],
+		OUTPUT_RANGE: [0, -15, 0, 15, 0, -15, 0, 15, 0],
+	}), []);
 
 	const [connected, setConnected] = useState(null);
 	const animation = useRef(new Animated.Value(0)).current;
@@ -28,48 +26,43 @@ const OfflineBar = ({bgColor='#930F1F', color="#fff", title='Please connect to W
 	useEffect(() => {
 		const checkInitialConnection = async () => {
 			const state = await NetInfo.fetch();
-			const { isConnected } = state;
-			console.log('Initial connectivity:', isConnected);
-			setConnected(isConnected);
+			setConnected(state.isConnected);
 		};
 
 		checkInitialConnection();
 
 		const unsubscribe = NetInfo.addEventListener(state => {
-			const { isConnected } = state;
-			console.log('Connectivity changed:', isConnected);
-			setConnected(isConnected);
+			setConnected(state.isConnected);
 			triggerAnimation();
 		});
 
-		return () => {
-			unsubscribe();
-		};
+		return () => unsubscribe();
 	}, [triggerAnimation]);
 
 	const interpolated = animation.interpolate({
 		inputRange: animationConstants.INPUT_RANGE,
 		outputRange: animationConstants.OUTPUT_RANGE,
 	});
+
 	const animationStyle = {
 		transform: [{ translateX: interpolated }],
 	};
-  
-	return connected != null && !connected ? (
-		<SafeAreaView style={{...styles.container, backgroundColor: bgColor}}>
+
+	return connected !== null && !connected ? (
+		<SafeAreaView style={[styles.container, { backgroundColor: bgColor }]}>
 			<StatusBar backgroundColor={bgColor} />
-			<Animated.Text style={{...styles.offlineText, ...animationStyle, color:color}}>
+			<Animated.Text style={[styles.offlineText, animationStyle, { color }]}>
 				{title}
 			</Animated.Text>
 		</SafeAreaView>
 	) : null;
 };
 
-const styles =  StyleSheet.create({
+const styles = StyleSheet.create({
 	container: {
 		paddingTop: 16,
 	},
-	statusText: {
+	offlineText: {
 		padding: 8,
 		textAlign: 'center',
 		fontWeight: '500',
